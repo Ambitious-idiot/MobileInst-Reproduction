059ff4bf87d82ee5a7101326f95b1572245ff2d2
YhSong <1308293139@qq.com>
2023-06-28 23:28:16 +0800

    deleted deprecated parts

--- sbin/train.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-root=$(dirname $0)/../../datasets/VOC2012/VOCdevkit/VOC2012
-checkpoint_root=$(dirname $0)/../checkpoints
-checkpoint=$(ls $checkpoint_root | sort -r | head -n 1)
-python -u  $(dirname $0)/../train.py --root $root --checkpoint_root $checkpoint_root --resume True --checkpoint $checkpoint
--- sbin/val.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-root=$(dirname $0)/../../datasets/VOC2012/VOCdevkit/VOC2012
-checkpoint_root=$(dirname $0)/../checkpoints
-out_root=$(dirname $0)/../out
-checkpoint=$(ls $checkpoint_root | sort -r | head -n 1)
-python -u $(dirname $0)/../val.py --root $root --checkpoint_root $checkpoint_root --checkpoint $checkpoint --out_root $out_root
